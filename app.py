3e689c8a8090917fb536ed00eb3739efd171a63d
yeojiny7 <kimyeojiny7@gmail.com>
2024-08-21 16:37:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ import streamlit as st
 st.title("타이틀")
 st.header("헤더")
 st.subheader("서브헤더")
-st.markdown(red[마크다운])
+st.markdown(''':red[마크다운]''')
 st.write("롸이트")
+
+isClicked = st.button("버튼")
+if isClicked :
+  st.write("클릭함")
