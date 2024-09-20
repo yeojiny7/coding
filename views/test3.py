3354abc924c04a9cd81cade863941f00bd96b7a0
yeojiny7 <kimyeojiny7@gmail.com>
2024-09-20 16:26:05 +0900

    Update test3.py

--- views/test3.py
+++ views/test3.py
@@ -3,5 +3,15 @@ import streamlit as st
 st.title("동전 던지기 게임")
 st.divider()
 
-st.image('assets/coin_head.png') st.image('assets/coin_tail.png')
+st.image('assets/coin_head.png')
+st.image('assets/coin_tail.png')
 
+st.header('동전 던지게 게임에 오신 것을 환영합니다.')
+st.subheader('앞면일까요? 뒷면일까요?')
+
+if st.button('앞면'):
+  st.text('앞면일 것 같음')
+
+
+if st.button('뒷면'):
+  st.text('뒷면일 것 같음')
