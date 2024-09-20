edbd21d8bddff83761876ff19db4c02955643c24
yeojiny7 <kimyeojiny7@gmail.com>
2024-09-20 16:21:22 +0900

    Update test3.py

--- views/test3.py
+++ views/test3.py
@@ -3,6 +3,5 @@ import streamlit as st
 st.title("동전 던지기 게임")
 st.divider()
 
-st.image('assets/coin_head.png')
-st.image('assets/coin_tail.png')
+st.image('assets/coin_head.png') st.image('assets/coin_tail.png')
 
